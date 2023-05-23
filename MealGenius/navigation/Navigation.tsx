6175a3f891a132d95bf0b7d45a389e92f0d0c450
Jami infante <jami@MacBook.local>
2023-05-23 16:33:46 +0200

    UPD : icons on bottom navbar

--- MealGenius/navigation/Navigation.tsx
+++ MealGenius/navigation/Navigation.tsx
@@ -4,6 +4,9 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import SettingsScreen from "../screens/SettingsScreen";
 import PantryScreen from "../screens/PantryScreen";
 import FavoriteScreen from "../screens/FavoriteScreen";
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 export default function Navigation() {
     const BottomTabNavigator = createBottomTabNavigator();
@@ -13,18 +16,46 @@ export default function Navigation() {
                 <BottomTabNavigator.Screen name="Home" component={HomeScreen}
                                            options={{
                                                title: 'Home',
+                                               tabBarIcon: ({ focused, color, size }) => (
+                                                   <Ionicons
+                                                       name={focused ? 'ios-home' : 'ios-home-outline'}
+                                                       size={size}
+                                                       color={color}
+                                                   />
+                                               ),
                                            }}/>
                 <BottomTabNavigator.Screen name="Pantry" component={PantryScreen}
                                            options={{
                                                title: 'Pantry',
+                                               tabBarIcon: ({ focused, color, size }) => (
+                                                   <Ionicons
+                                                       name={focused ? 'ios-restaurant' : 'ios-restaurant-outline'}
+                                                       size={size}
+                                                       color={color}
+                                                   />
+                                               ),
                                            }}/>
                 <BottomTabNavigator.Screen name="Favorite" component={FavoriteScreen}
                                            options={{
                                                title: 'Favorite',
+                                                  tabBarIcon: ({ focused, color, size }) => (
+                                                    <Ionicons
+                                                        name={focused ? 'ios-heart' : 'ios-heart-outline'}
+                                                        size={size}
+                                                        color={color}
+                                                    />
+                                                  ),
                                            }}/>
                 <BottomTabNavigator.Screen name="Settings" component={SettingsScreen}
                                            options={{
                                                title: 'Settings',
+                                                  tabBarIcon: ({ focused, color, size }) => (
+                                                    <Ionicons
+                                                        name={focused ? 'ios-settings' : 'ios-settings-outline'}
+                                                        size={size}
+                                                        color={color}
+                                                    />
+                                                  ),
                                            }}/>
             </BottomTabNavigator.Navigator>
         </NavigationContainer>
