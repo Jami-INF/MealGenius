7af17ceb4850105f712cc3ed9be4554396802bd4
Julien THEME <julien.theme@etu.uca.fr>
2023-05-27 21:00:55 +0200

    ✨add custome image

---
+++ MealGenius/components/CustomImage.tsx
@@ -0,0 +1,36 @@
+import { Image } from "react-native/types";
+
+type ImageType = "icon" | "meal";
+type ImageExtension = "png" | "jpg" | "jpeg";
+
+type ImageProps = {
+    imageType: ImageType,
+    imageName: string,
+    imageExtension: ImageExtension
+}
+
+/** Display an image with its type, name and extension.
+ * @param imageType The type of the image (`icon` or `meal`).
+ * @param imageName The name of the image.
+ * @param imageExtension The extension of the image (`png`, `jpg` or `jpeg`).
+ * @returns Return the image.
+ * @example
+ * <CustomImage imageType="icon" imageName="home" imageExtension="png"/>
+ * <CustomImage imageType="meal" imageName="hamburger" imageExtension="jpg"/>
+ */
+export default function CustomImage(props: ImageProps) {
+    return (
+        <Image source={require(getPath(props.imageType, props.imageName, props.imageExtension))}/>
+    )
+}
+
+/** Get the path of an image with its type, name and extension.
+ * @param imageType The type of the image (`icon` or `meal`).
+ * @param imageName The name of the image.
+ * @param imageExtension The extension of the image (`png`, `jpg` or `jpeg`).
+ * @returns Return the path of the image.
+ */
+function getPath(imageType: ImageType, imageName: string, imageExtension: ImageExtension): string {
+    let folder: string = imageType === "icon" ? "icons" : "meals";
+    return `../assets/${folder}/${imageName}${imageExtension}`;
+}
