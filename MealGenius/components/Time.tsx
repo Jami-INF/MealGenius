c54fb4341b0bbdbc9759e817e3e9d3fdd7ea5171
Julien THEME <julien.theme@etu.uca.fr>
2023-05-29 21:16:45 +0200

    ♻️update time component
    - hour in h
    - minutes in min

--- MealGenius/components/Time.tsx
+++ MealGenius/components/Time.tsx
@@ -1,24 +1,24 @@
 import { Text } from "react-native";
 
 type TimeProps = {
-    time: number
+    time: number,
+    fontSize?: number
 }
 
 /** Transform the time in minutes to a string.
  * @param props The time in minutes.
  * @example
  * <Time time={0}/> => ""
- * <Time time={1}/> => "1 minute"
- * <Time time={2}/> => "2 minutes"
- * <Time time={60}/> => "1 hour"
- * <Time time={120}/> => "2 hours"
- * <Time time={61}/> => "1 hour 1 minute"
- * <Time time={121}/> => "2 hours 2 minutes"
- * 
+ * <Time time={1}/> => "1 min"
+ * <Time time={2}/> => "2 min"
+ * <Time time={60}/> => "1h"
+ * <Time time={120}/> => "2h"
+ * <Time time={121}/> => "2 h 1 min"
+ * <Time time={122}/> => "2 h 2 min"
  */
 export default function Time(props: TimeProps): JSX.Element {
     return (
-        <Text>{getTimeText(props.time)}</Text>
+        <Text style={{fontSize: props.fontSize}}>{getTimeText(props.time)}</Text>
     )
 }
 
@@ -26,46 +26,38 @@ export default function Time(props: TimeProps): JSX.Element {
  * @param time The time in minutes.
  * @example 
  * getHoursText(0) => ""
- * getHoursText(1) => "1 hour"
- * getHoursText(2) => "2 hours"
+ * getHoursText(1) => "1 h"
+ * getHoursText(2) => "2 h"
  */
 function getHoursText(time: number): string {
     let hours: number = Math.floor(time / 60);
 
-    if (hours <= 0)
-        return ""
-    else 
-        return hours === 1 ? `${hours} hour` : `${hours} hours`;
+    return hours <= 0 ? "" : `${hours} h`
 }
 
 /** Get the minutes written in text.
  * @param time The time in minutes.
  * @example
  * getMinutesText(0) => ""
- * getMinutesText(1) => "1 minute"
- * getMinutesText(2) => "2 minutes"
+ * getMinutesText(1) => "1 min"
+ * getMinutesText(2) => "2 min"
  */
 function getMinutesText(time: number): string {
     let minutes: number = time % 60;
 
-    if (minutes <= 0)
-        return ""
-    else 
-        return minutes === 1 ? `${minutes} minute` : `${minutes} minutes`;
+    return minutes <= 0 ? "" : `${minutes} min`
 }
 
 /** Get the time written in text.
  * @param time The time in minutes.
  * @example
  * getTimeText(0) => ""
- * getTimeText(1) => "1 minute"
- * getTimeText(2) => "2 minutes"
- * getTimeText(60) => "1 hour"
- * getTimeText(61) => "1 hour 1 minute"
- * getTimeText(62) => "1 hour 2 minutes"
- * getTimeText(120) => "2 hours"
- * getTimeText(121) => "2 hours 1 minute"
- * getTimeText(122) => "2 hours 2 minutes"
+ * getTimeText(1) => "1 min"
+ * getTimeText(2) => "2 min"
+ * getTimeText(60) => "1 h"
+ * getTimeText(120) => "2 h"
+ * getTimeText(121) => "2 h 1 min"
+ * getTimeText(122) => "2 h 2 min"
  */
 function getTimeText(time: number): string {
     return `${getHoursText(time)} ${getMinutesText(time)}`;
