b03bb5e78a3edba3b9dfbfab91a37e3f50c65a2b
Jami infante <jami@MacBook.lan>
2023-06-17 22:06:42 +0200

    UPD : MealDetailsScreen

--- MealGenius/screens/MealDetailsScreen.tsx
+++ MealGenius/screens/MealDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Text, View, FlatList } from "react-native";
+import { Image, StyleSheet, View, FlatList } from "react-native";
 import { Meal } from "../models/Meal";
 import { ScrollView } from "react-native-gesture-handler";
 import BackButton from "../components/BackButton";
@@ -9,7 +9,6 @@ import { Ionicons } from "@expo/vector-icons";
 import React, { useEffect, useState } from "react";
 import { Ingredient } from "../models/Ingredient";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {Step} from "../models/Step";
 
 type MealDetailsScreenProps = {
     route: {
@@ -18,8 +17,8 @@ type MealDetailsScreenProps = {
         };
     };
 };
+export default function MealDetailsScreen(props: MealDetailsScreenProps): JSX.Element  {
 
-const MealDetailsScreen = (props: MealDetailsScreenProps): JSX.Element => {
     const [isFavorite, setIsFavorite] = useState(false); // État du plat favori
 
     useEffect(() => {
@@ -29,16 +28,16 @@ const MealDetailsScreen = (props: MealDetailsScreenProps): JSX.Element => {
     const getFavorites = async () => {
         try {
             const jsonFavorite = await AsyncStorage.getItem('favorites')
-            if(jsonFavorite != null){
+            if (jsonFavorite != null) {
                 const jsonFavorites = JSON.parse(jsonFavorite)
-                const favorites : Meal[] = [];
-                for(let i = 0; i < jsonFavorites.length; i++){
-                    let m : Meal = new Meal(jsonFavorites[i]._id, jsonFavorites[i]._name, jsonFavorites[i]._description, jsonFavorites[i]._image, jsonFavorites[i]._duration, jsonFavorites[i]._ingredients, jsonFavorites[i]._type, jsonFavorites[i]._steps, jsonFavorites[i]._complexity);
+                const favorites: Meal[] = [];
+                for (let i = 0; i < jsonFavorites.length; i++) {
+                    let m: Meal = new Meal(jsonFavorites[i]._id, jsonFavorites[i]._name, jsonFavorites[i]._description, jsonFavorites[i]._image, jsonFavorites[i]._duration, jsonFavorites[i]._ingredients, jsonFavorites[i]._type, jsonFavorites[i]._steps, jsonFavorites[i]._complexity);
                     favorites.push(m);
                 }
                 return favorites;
             }
-        } catch(e) {
+        } catch (e) {
             console.log("An error occurred", e);
         }
     }
@@ -57,28 +56,6 @@ const MealDetailsScreen = (props: MealDetailsScreenProps): JSX.Element => {
             console.log("Error retrieving favorites from AsyncStorage: ", error);
         }
     };
-
-export default function MealDetailsScreen(props: MealDetailsScreenProps): JSX.Element  {
-
-    return (
-        <View style={styles.container}>
-            <BackButton/>
-            <Portal>
-                <IconButton icon="star" 
-                            onPress={() => console.log("Favorite button pressed")}
-                            size={30}
-                            iconColor="black"
-                            mode="contained-tonal"
-                            style={styles.favoriteButton}
-                            animated={true}/>
-            </Portal>
-            
-            <Image style={styles.image} source={{uri: props.route.params.meal.image}}/>
-
-            <ScrollView style={styles.scrollview}
-                        bounces={false}>
-              
-                <View style={styles.body}>  
     const addToFavorites = async (plat: Meal) => {
         try {
             const favorites = await getFavorites();
@@ -135,146 +112,158 @@ export default function MealDetailsScreen(props: MealDetailsScreenProps): JSX.El
         }
     };
 
+
+        return (
+            <View style={styles.container}>
+                <BackButton/>
+                <Portal>
+                    <IconButton
+                        icon={isFavorite ? "star" : "star-outline"} // Utiliser le bon logo en fonction de l'état du plat favori
+                        onPress={handleFavoritePress}
+                        size={30}
+                        iconColor="black"
+                        mode="contained-tonal"
+                        style={styles.favoriteButton}
+                        animated={true}
+                    />
+                </Portal>
+
+                <Image style={styles.image} source={{uri: props.route.params.meal.image}}/>
+
+                <ScrollView style={styles.scrollview}
+                            bounces={false}>
+
+                    <View style={styles.body}>
+                        <View style={styles.title}>
+                            <CustomText text={props.route.params.meal.name} textType="title"/>
+                        </View>
+
+                        <View style={styles.mealInformation}>
+                            <CustomText text={"Ingrédients"} textType="subtitle"/>
+                            <FlatList data={props.route.params.meal.ingredients}
+                                      renderItem={({item}) => <Text style={styles.ingredient} >{getIngredientText(item)}</Text>}
+                                      style={styles.ingredients}
+                                      scrollEnabled={false}/>
+
+                            <Divider style={styles.divider}/>
+
+                            <CustomText text={"Préparation"} textType="subtitle"/>
+
+                            <FlatList data={props.route.params.meal.steps}
+                                      style={styles.steps}
+                                      scrollEnabled={false}
+                                      renderItem={({item, index}) =>
+                                          <Surface style={styles.card}>
+                                              <View>
+                                                  <View style={styles.cardHeader}>
+                                                      <Text variant="titleLarge" style={styles.titlesStep}>{`Etape ${index + 1}`}</Text>
+                                                      <View style={styles.clock}>
+                                                          <Ionicons name="time-outline" size={20}/>
+                                                          <Time time={item.duration} fontSize={17}/>
+                                                      </View>
+                                                  </View>
+
+                                                  <Text style={styles.step}>{item.description}</Text>
+                                              </View>
+                                          </Surface>}/>
+
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
+
+        );
+    }
+
     function getIngredientText(ingredient: Ingredient): string {
         return `- ${ingredient.quantity} ${ingredient.unit} de ${ingredient.food.name}`;
     }
 
-    return (
-        <View style={styles.container}>
-            <BackButton />
-            <Image style={styles.image} source={{ uri: props.route.params.meal.image }} />
-            <ScrollView style={styles.scrollview} bounces={false}>
-                <View style={styles.body}>
-                    <View style={styles.title}>
-                        <CustomText text={props.route.params.meal.name} textType="title" />
-                    </View>
-
-                    <View style={styles.mealInformation}>
-                        <CustomText text={"Ingrédients"} textType="subtitle"/>
-                        <FlatList data={props.route.params.meal.ingredients}
-                                renderItem={({item}) => <Text style={styles.ingredient} >{getIngredientText(item)}</Text>}
-                                style={styles.ingredients}
-                                scrollEnabled={false}/>
-                        
-                        <Divider style={styles.divider}/>
-
-                        <CustomText text={"Préparation"} textType="subtitle"/>
-                        
-                        <FlatList data={props.route.params.meal.steps}
-                                style={styles.steps}
-                                scrollEnabled={false}
-                                renderItem={({item, index}) => 
-                                    <Surface style={styles.card}>
-                                        <View>
-                                            <View style={styles.cardHeader}>
-                                                <Text variant="titleLarge" style={styles.titlesStep}>{`Etape ${index + 1}`}</Text>
-                                                <View style={styles.clock}>
-                                                    <Ionicons name="time-outline" size={20}/>
-                                                    <Time time={item.duration} fontSize={17}/>
-                                                </View>
-                                            </View>
-                                            
-                                            <Text style={styles.step}>{item.description}</Text>
-                                        </View>
-                                    </Surface>}/>
-                        
-
-                    </View>
-                </View>
-            </ScrollView>
-        </View>
-    );
-};
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    image: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: 250,
-        zIndex: -1,
-    },
-    body: {
-        borderRadius: 20,
-        backgroundColor: "white",
-        marginTop: 230,
-        minHeight: 700
-        height: 1000,
-    },
-    scrollview: {
-        backgroundColor: "transparent",
-        width: '100%'
-    },
-    title: {
-        alignItems: "center",
-        marginVertical: 20
-
-    },
-    mealInformation: {
-        marginHorizontal: 30,
-    },
-    ingredient: {
-        marginVertical: 10,
-        marginLeft: 10,
-    },
-    ingredients: {
-        marginTop: 10,
-        marginBottom: 20
-    },
-    steps: {
-        marginTop: 10,
-    },
-    step: {
-        marginVertical: 10,
-        marginHorizontal: 10
-    },
-    titleStep: {
-        marginVertical: 10,
-        marginLeft: 10,
-        fontSize: 20,
-        fontWeight: "bold"
-    },
-    titlesStep: {
-        marginTop: 5,
-        marginBottom: 10,
-        marginHorizontal: 10,
-    },
-    card: {
-        width: "auto",
-        borderRadius: 20,
-        flexDirection: "row",
-        minHeight: 50,
-        minWidth: "100%",
-        height: "auto",
-        marginHorizontal: 3,
-        marginVertical: 10
-    },
-    divider: {
-        marginVertical: 10
-    },
-    clock: {
-        flexDirection: "row",
-        alignItems: "center",
-        marginHorizontal: 10
-    },
-    cardHeader: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        alignContent: "center",
-        maxWidth: "100%",
-        minWidth: "100%"
-    },
-    favoriteButton: {
-        margin: 10,
-        position: "absolute",
-        top: 0,
-        right: 0,
-    }
-}); 
-
-export default MealDetailsScreen;
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1
+        },
+        image: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: 250,
+            zIndex: -1,
+        },
+        body: {
+            borderRadius: 20,
+            backgroundColor: 'white',
+            marginTop: 230,
+            minHeight: 700
+        },
+        scrollview : {
+            backgroundColor: "transparent",
+            width: '100%'
+        },
+        title: {
+            alignItems: "center",
+            marginVertical: 20
+        },
+        mealInformation: {
+            marginHorizontal: 30,
+        },
+        ingredient: {
+            marginVertical: 10,
+            marginLeft: 10,
+        },
+        ingredients: {
+            marginTop: 10,
+            marginBottom: 20
+        },
+        steps: {
+            marginTop: 10
+        },
+        step: {
+            marginVertical: 10,
+            marginHorizontal: 10
+        },
+        titleStep: {
+            marginVertical: 10,
+            marginLeft: 10,
+            fontSize: 20,
+            fontWeight: "bold"
+        },
+        titlesStep: {
+            marginTop: 5,
+            marginBottom: 10,
+            marginHorizontal: 10,
+        },
+        card: {
+            width: "auto",
+            borderRadius: 20,
+            flexDirection: "row",
+            minHeight: 50,
+            minWidth: "100%",
+            height: "auto",
+            marginHorizontal: 3,
+            marginVertical: 10
+        },
+        divider: {
+            marginVertical: 10
+        },
+        clock: {
+            flexDirection: "row",
+            alignItems: "center",
+            marginHorizontal: 10
+        },
+        cardHeader: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            alignContent: "center",
+            maxWidth: "100%",
+            minWidth: "100%"
+        },
+        favoriteButton: {
+            margin: 10,
+            position: "absolute",
+            top: 0,
+            right: 0,
+        }
+    });
