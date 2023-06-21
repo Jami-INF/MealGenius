beba76cd0cb52cf4007b36dbc99d8d20d0fa1a3f
Julien THEME <julien.theme@etu.uca.fr>
2023-06-21 23:53:07 +0200

    ✅ add action tests

--- MealGenius/tests/actionMealList.test.ts
+++ MealGenius/tests/actionMealList.test.ts
@@ -1,31 +1,36 @@
-import { useSelector } from "react-redux";
 import { Meal } from "../models/Meal";
-import { getMealList } from "../redux/actions/actionMealList";
+import { getMealList, setMealList } from "../redux/actions/actionMealList";
 import { getMeals } from "../stub/stub";
+import { FETCH_MEAL_LIST } from "../redux/constants";
 
-describe('test actions', () => {
+describe('MealList Actions - FETCH_MEAL_LIST', () => {
+    // Mock meal data
+    const meals: Meal[] = getMeals();
+      
+    // Expected action
+    const expectation = {
+      type: FETCH_MEAL_LIST,
+      payload: meals,
+    };
 
-    it('should create an action with FETCH_MEAL_LIST type', () => {
-        const meals: Meal[] = getMeals();
-        const expectation = {
-            type: 'FETCH_MEAL_LIST',
-            nounours: meals,
-        };
-        
-        expect(useSelector(getMeals)).toEqual(expectation);
-    })
-
-    it('should create an action with ADD_FAVORITE_MEALS type', () => {
-
-        const meal: Meal = getMeals()[0];
-
-        const expectation = {
-            type: 'ADD_FAVORITE_MEALS',
-            nounours: meal,
-        };
-        
-        expect(getMealList()).toEqual(expectation);
-    })
-
-    
+    it("should create an action with FETCH_MEAL_LIST type for setMealList", () => {
+        // Test setMealList action
+        expect(setMealList(meals)).toEqual(expectation);
+      });
+      
+      it("should dispatch an action with FETCH_MEAL_LIST type for getMealList", () => {
+        // Mock fetch response
+        const dispatchMock = jest.fn();
+        const fetchMock = jest.fn().mockResolvedValue({
+          status: 200,
+          json: jest.fn().mockResolvedValue(meals),
+        });
+      
+        global.fetch = fetchMock;
+      
+        // Test getMealList action
+        return getMealList()(dispatchMock).then(() => {
+          expect(dispatchMock).toHaveBeenCalledWith(expectation);
+        });
+    });
 })
